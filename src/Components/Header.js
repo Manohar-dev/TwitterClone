eb9738fde95e19972ca57b372a939b90ee55c0f3
Manohar Vempadapu <manohariitkharagpur@gmail.com>
2024-01-30 13:17:21 +0530

    Initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+
+import  React from 'react';
+import SideNav from './SideNav';
+
+const Header=()=> {
+
+  return (
+    <div className='header'>
+    <SideNav/>
+    
+    </div>
+  );
+}
+export default Header;
+
+
+
+
---
+++ src/Components/UserFeed.js
@@ -0,0 +1,10 @@
+import React from 'react'
+̥import React, { Component } from 'react'
+
+export default class UserFeed extends Component {
+  render() {
+    return (
+      <div>UserFeed</div>
+    )
+  }
+}
